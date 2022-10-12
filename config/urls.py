a0b631659f3e1fd25049c5f5905e7c465863760f
fayzullox07 <90905132+fayzullox07@users.noreply.github.com>
2022-10-12 18:08:59 +0500

    new file

--- config/urls.py
+++ config/urls.py
@@ -11,11 +11,20 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("main.urls", namespace="main")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField("Nomi", max_length=35)
+    url = models.CharField("Manzil (URL)", max_length=25)
+    img = models.ImageField(upload_to="product-imagae/")
+    desc = models.CharField("Haqida", max_length=30)
+    like = models.PositiveIntegerField(default=0)
+    view = models.PositiveIntegerField(default=0)
+    class Meta:
+        verbose_name = "Project"
+        verbose_name_plural = "Projects"
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse("Project_detail", kwargs={"pk": self.pk})
+
+
+  
